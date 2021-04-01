bb8e0a176d56c5e37da1ae66a26da2557d46bf86
gab1005 <gabrielqwerty1@gmail.com>
2021-04-01 14:01:54 -0300

    projeto DeMolay Quiz

---
+++ pages/api/db.js
@@ -0,0 +1,14 @@
+// e uma ferramenta do next que permite chamar o db.json
+// dessa forma fica acessivel ver os dados pelo navegador
+export default function dbHandler(request, response) {
+  if (request.method === 'OPTIONS') {
+    response.status(200).end();
+    return;
+  }
+
+  response.setHeader('Access-Control-Allow-Credentials', true);
+  response.setHeader('Access-Control-Allow-Origin', '*');
+  response.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+
+  response.json(db);
+}
